afdebaa354b7615ff10d430e4e38ed28052f48cf
Divo Tanudjaja <57279041+DivoTanudjaja@users.noreply.github.com>
2021-06-05 11:16:27 +0700

    Added add admin

--- vaccineproject/vaccineproject/urls.py
+++ vaccineproject/vaccineproject/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls.conf import include, path
 from web.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('add_admin/', SignUpView.as_view(), name='adduser'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
